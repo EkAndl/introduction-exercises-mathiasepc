e54a861a670a41c7d3c2a67f1088d1c35b7f0646
mathi <mathiasepc@msn.com>
2026-01-29 12:48:57 +0100

    done

--- 2._Strings.js
+++ 2._Strings.js
@@ -7,13 +7,18 @@ const numberTwo = "2.30";
 // add those two numbers and show the result
 // you cannot touch line 1 neither line 2
 
+const result = Number(numberOne) + Number(numberTwo);
 
+console.log(result);
 // --------------------------------------
 // Exercise 4 - Add the numbers and the total with 2 decimals
 
 const anotherNumberOne = "1.10";
 const anotherNumberTwo = "2.30";
 
+const result2 = Number(anotherNumberOne) + Number(anotherNumberTwo);
+
+console.log(`${anotherNumberOne} + ${anotherNumberTwo} = ${result2}`)
 
 // --------------------------------------
 // Exercise 5 - Decimals and average
@@ -24,8 +29,11 @@ const three = 98;
 
 // Show in the console the avg. with 5 decimals
 
+const sumAvg = one + two + three / 3;
 
+const avgWithDecimal = Number(sumAvg).toFixed(5) 
 
+console.log(avgWithDecimal);
 
 // --------------------------------------
 // Exercise 6 - Get the character by index
@@ -34,7 +42,9 @@ const letters = "abc";
 // Get me the character "c"
 
 
+const last = letters.substring(2);
 
+console.log(last);
 
 // --------------------------------------
 // Exercise 7 - Replace
@@ -43,6 +53,8 @@ const fact = "You are learning javascript!";
 
 // capitalize the J in Javascript
 
+const capitalize = fact.replace("j", "J");
+console.log(capitalize);
 
 // --------------------------------------
 
