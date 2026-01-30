5f6eb46c25246b16171616513a4c403a89c58831
mathi <mathiasepc@msn.com>
2026-01-30 09:58:31 +0100

    Changed exercise 7 - better naming 1.1

--- 4._Arrays.js
+++ 4._Arrays.js
@@ -92,9 +92,9 @@ const lettersExpanded = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 // log every second char in the array starting from b
 
-lettersExpanded.forEach((k,i) => {
-    if(!k.includes("a") && (i+1)%2== 0) 
-        console.log(k);
+lettersExpanded.forEach((v,i) => {
+    if(!v.includes("a") && (i+1)%2== 0) 
+        console.log(v);
     });
 
 
